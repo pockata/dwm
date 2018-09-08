12af2670f666a4f94daeedc10a17d74ec144132b
Rosen Stoyanov <me@pockata.org>
2018-09-08 23:54:10 +0300

    Apply `centered` patch
    https://dwm.suckless.org/patches/center/

--- config.h
+++ config.h
@@ -28,12 +28,9 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",         NULL,       NULL,       0,            1,           -1 },
-	{ "Nightly",      NULL,       NULL,       1 << 1,       1,           -1 },
-	{ "Alacritty",    NULL,       NULL,       1 << 0,       1,           -1 },
-	{ "Pavucontrol",  NULL,       NULL,       1 << 0,       1,           -1 },
-	{ "mpv",          NULL,       NULL,       1 << 0,       1,           0 },
+	/* class      instance    title       tags mask     iscentered     isfloating   monitor */
+	{ "Nightly",      NULL,       NULL,       1 << 1,       1,              1,           -1 },
+	{ "mpv",          NULL,       NULL,       0,            1,              1,            0 },
 };
 
 /* layout(s) */
