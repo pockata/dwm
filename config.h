57b57c5e1928fc3d303ac3159c2f4fed986921c1
Rosen Stoyanov <me@pockata.org>
2018-09-09 00:22:21 +0300

    Apply `viewontag` patch
    https://dwm.suckless.org/patches/viewontag/

--- config.h
+++ config.h
@@ -3,10 +3,11 @@
 /* appearance */
 static const unsigned int borderpx  = 6;        /* border pixel of windows */
 static const unsigned int gappx     = 18;       /* gap pixel between windows */
-static const unsigned int snap      = 4;       /* snap pixel */
+static const unsigned int snap      = 4;        /* snap pixel */
 static const int focusonwheel       = 0;
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int viewontag          = 1;        /* Switch view on tag switch */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
