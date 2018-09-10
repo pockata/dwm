00dc95c5c3359a5a43535d350467f5f1c29393db
Rosen Stoyanov <me@pockata.org>
2018-09-10 12:33:38 +0300

    Add multi-monitor support to `centered` patch

--- dwm.c
+++ dwm.c
@@ -1035,10 +1035,9 @@ manage(Window w, XWindowAttributes *wa)
 	c->bw = borderpx;
 
 	if(c->iscentered) {
-		c->x = (c->mon->mw - WIDTH(c)) / 2;
-		c->y = (c->mon->mh - HEIGHT(c)) / 2;
+		c->x = c->mon->mx + (c->mon->mw - WIDTH(c)) / 2;
+		c->y = c->mon->my + (c->mon->mh - HEIGHT(c)) / 2;
 	}
-
 
 	wc.border_width = c->bw;
 	XConfigureWindow(dpy, w, CWBorderWidth, &wc);
